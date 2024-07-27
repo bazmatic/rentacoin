f716164d62a683407c464277c040d098948043f3
Barry Earsman <barry@earsman.com>
2024-07-27 22:20:17 +1000

    feat: simpler styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import AdSpaceRentalDApp from '../components/AdSpaceRentalDApp';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="main-content">
       <AdSpaceRentalDApp />
     </main>
   );
