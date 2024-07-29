6e77029f1110673027aeae009afd9d04331f9965
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-07-30 08:44:18 +1000

    chore: formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
-import { VT323 } from 'next/font/google'
+import { VT323 } from "next/font/google";
 import "./globals.css";
 
-const inter = VT323({ subsets: ['latin'], weight: ['400'] })
+const inter = VT323({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import AdSpaceRentalDApp from "../components/AdSpaceRentalDApp";
 
 export default function Home() {
-    return (
-        <div className="content-wrapper">
-            <main className="main-content">
-                <AdSpaceRentalDApp />
-            </main>
-        </div>
-    );
+  return (
+    <div className="content-wrapper">
+      <main className="main-content">
+        <AdSpaceRentalDApp />
+      </main>
+    </div>
+  );
 }
